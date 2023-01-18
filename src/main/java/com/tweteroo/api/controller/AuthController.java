7319caf75c2eeb40aa03d1bb6813ca10afc016e2
Isadora Ávila <isadorgravila@gmail.com>
2023-01-18 18:40:51 -0300

    feat: save new user in the repository

--- src/main/java/com/tweteroo/api/controller/AuthController.java
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -1,18 +1,27 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UserDTO;
+import com.tweteroo.api.model.User;
+import com.tweteroo.api.repository.UserRepository;
 
 @RestController
 @RequestMapping("/sign-up")
 public class AuthController {
+
+  @Autowired
+  private UserRepository repository;
   
   @PostMapping
   public void signUp(@RequestBody UserDTO req) {
-    System.out.println(req);
+    repository.save(new User(req));
   }
 }
