3355006afca292d76fee1062be3ad44cfccf53c5
Isadora Ávila <isadorgravila@gmail.com>
2023-01-18 15:37:58 -0300

    feat: create get methods

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,5 +1,6 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,15 @@ public class TweetController {
   @PostMapping
   public void createTweet(@RequestBody TweetDTO req) {
     System.out.println(req);
+  }
+
+  @GetMapping
+  public void listAll() {
+    System.out.println("get all tweets");
+  }
+
+  @GetMapping("/USERNAME")
+  public void listAllByUser() {
+    System.out.println("get all tweets by user");
   }
 }
