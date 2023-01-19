5c353f7525dbaeb6512c81dbff5e8790b348b322
Isadora Ávila <isadorgravila@gmail.com>
2023-01-19 16:42:06 -0300

    refactor: remove unused imports

--- src/main/java/com/tweteroo/api/controller/AuthController.java
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -1,9 +1,6 @@
 package com.tweteroo.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
