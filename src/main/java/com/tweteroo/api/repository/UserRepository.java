cc662914cc70f6032f6946b523831ae3c56b7778
Isadora Ávila <isadorgravila@gmail.com>
2023-01-19 16:43:20 -0300

    feat: add findByUsername method

--- src/main/java/com/tweteroo/api/repository/UserRepository.java
+++ src/main/java/com/tweteroo/api/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tweteroo.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo.api.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  List<User> findByUsername(String username);
   
 }
