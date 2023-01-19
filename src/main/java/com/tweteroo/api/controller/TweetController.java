da7adf34156c0c0bf39d971b93036b88403eb822
Isadora Ávila <isadorgravila@gmail.com>
2023-01-19 16:44:15 -0300

    feat: use the createTweet method to save tweet

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,5 +1,6 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweetDTO;
+import com.tweteroo.api.model.Tweet;
+import com.tweteroo.api.repository.TweetRepository;
+import com.tweteroo.api.repository.UserRepository;
 
 @RestController
 @RequestMapping("/tweets")
 public class TweetController {
 
+  @Autowired
+  private TweetRepository tweetRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
   @PostMapping
   public void createTweet(@RequestBody TweetDTO req) {
-    System.out.println(req);
+    String avatar = userRepository.findByUsername(req.username()).get(0).getAvatar();
+
+    //se não existir usuário, dar erro
+    
+    tweetRepository.save(new Tweet(req, avatar));
   }
 
   @GetMapping
