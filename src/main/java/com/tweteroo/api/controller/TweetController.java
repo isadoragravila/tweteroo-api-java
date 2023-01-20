e07c470bd6c825a9652b4f293ea071dcad2e2554
Isadora Ávila <isadorgravila@gmail.com>
2023-01-20 14:38:36 -0300

    feat: enable cors

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -3,6 +3,7 @@ package com.tweteroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.tweteroo.api.model.Tweet;
 import com.tweteroo.api.services.TweetService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/tweets")
 public class TweetController {
 
