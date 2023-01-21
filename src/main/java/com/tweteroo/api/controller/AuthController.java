f7d59c501abbd6229908454871d25f192cec09cd
Isadora Ávila <isadorgravila@gmail.com>
2023-01-21 19:04:49 -0300

    feat: add response entity and response status

--- src/main/java/com/tweteroo/api/controller/AuthController.java
+++ src/main/java/com/tweteroo/api/controller/AuthController.java
@@ -1,17 +1,19 @@
 package com.tweteroo.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.UserDTO;
 import com.tweteroo.api.services.UserService;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/auth/sign-up")
 public class AuthController {
 
@@ -19,6 +21,7 @@ public class AuthController {
   private UserService service;
   
   @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
   public void signUp(@RequestBody UserDTO req) {
     service.save(req);
   }
--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -3,12 +3,15 @@ package com.tweteroo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.TweetDTO;
@@ -16,7 +19,7 @@ import com.tweteroo.api.model.Tweet;
 import com.tweteroo.api.services.TweetService;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/tweets")
 public class TweetController {
 
@@ -29,11 +32,17 @@ public class TweetController {
   }
 
   @GetMapping("/{username}")
-  public List<Tweet> listAllByUser(@PathVariable String username) {
-    return service.findByUsername(username);
+  public ResponseEntity<List<Tweet>> listAllByUser(@PathVariable String username) {
+    var tweetsByUser = service.findByUsername(username);
+    if(tweetsByUser != null) {
+      return ResponseEntity.ok().body(tweetsByUser);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
   }
 
   @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
   public void createTweet(@RequestBody TweetDTO req) {
     service.save(req);
   }
